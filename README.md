d905c3340d657dff8468864a5376be489ddf05bc
delchev <delchevn@gmail.com>
2018-05-22 16:15:18 +0300

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Template V3 - Hello World with Tabris
+
+[![Eclipse License](http://img.shields.io/badge/license-Eclipse-brightgreen.svg)](LICENSE)
+[![GitHub contributors](https://img.shields.io/github/contributors/dirigiblelabs/template-v3-bookstore.svg)](https://github.com/dirigiblelabs/template-v3-bookstore/graphs/contributors)
+
+
+## Overview
+
+Simple Hello World mobile application powered by Tabris.
+
+
+## License
+
+This project is copyrighted by [SAP SE](http://www.sap.com/) and is available under the [Eclipse Public License v 1.0](https://www.eclipse.org/legal/epl-v10.html). See [LICENSE](LICENSE) and [NOTICE.txt](NOTICE.txt) for further details.
---
+++ template-mobile-hello-world/template.js
@@ -0,0 +1,26 @@
+exports.getTemplate = function() {
+	return {
+		"name": "Hello World with Tabris",
+		"description": "Hello World Mobile Application with Tabris",
+		"sources": [
+		{
+			"location": "/template-mobile-hello-world/app.js.template", 
+			"action": "generate",
+			"rename": "app.js"
+		},
+		{
+			"location": "/template-mobile-hello-world/package.json.template", 
+			"action": "generate",
+			"rename": "package.json"
+		},
+		{
+			"location": "/template-mobile-hello-world/node_modules/tabris/package.json", 
+			"action": "copy"
+		},
+		{
+			"location": "/template-mobile-hello-world/node_modules/tabris/tabris.min.js", 
+			"action": "copy"
+		}],
+		"parameters": []
+	};
+};
